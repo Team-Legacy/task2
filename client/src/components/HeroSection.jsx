fb8fa4c8e0ebce45e9ff65df125e710b5bc7e20e
am-miracle <judemiraco0@gmail.com>
2022-04-12 23:27:46 +0100

    UI update

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -1,15 +1,65 @@
+//jshint esversion:8
 import {
-  // Button,
+  Box,
   Flex,
   Heading,
   Image,
+  SlideFade,
   Stack,
   Text,
   useBreakpointValue,
 } from '@chakra-ui/react';
 import Hero from '../assets/undraw_online_organizer_re_156n.svg'
+import Upload from '../assets/undraw_upload_re_pasx.svg';
+import Share from '../assets/undraw_share_re_9kfx.svg';
 import Button from './button'
+import { useEffect, useRef, useState,useCallback } from 'react';
+
+
+const images = [Hero,Upload, Share];
+const texts = ["Retrieve files","Upload files ", "Share files with other users"];
+const delay = 3000;
+
+
 export default function HeroSection() {
+  const [index, setIndex] = useState(0);
+  const [newText, setnewText] = useState("");
+  const timeoutRef = useRef(null);
+
+  const resetTimeout =() =>{
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+  }
+  const shuffle = useCallback(() => {
+    const index = Math.floor(Math.random() * texts.length);
+    setnewText(texts[index]);
+  }, []);
+
+
+  useEffect(() => {
+    resetTimeout();
+    timeoutRef.current = setTimeout(
+      () =>
+        setIndex((prevIndex) =>
+          prevIndex === images.length - 1 ? 0 : prevIndex + 1
+        ),
+        // setnewText((prevIndex) =>
+        //   prevIndex === texts.length -1 ? 0 : prevIndex + 1
+        // ),
+      delay
+    );
+    const intervalID = setInterval(shuffle, 3000);
+
+    return () => {
+      resetTimeout();
+      clearInterval(intervalID);
+    };
+  }, [
+    index, 
+    shuffle
+  ]);
+
   return (
     <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
       <Flex p={8} flex={1} align={'center'} justify={'center'}>
@@ -25,7 +75,6 @@ export default function HeroSection() {
                 position: 'absolute',
                 bottom: 1,
                 left: 0,
-                // bg: 'orange.400',
                 zIndex: -1,
               }}>
               Develop your
@@ -35,20 +84,37 @@ export default function HeroSection() {
               Emaginatioin
             </Text>{' '}
           </Heading>
-          <Text fontSize={{ base: 'md', lg: 'lg' }} color={'gray.500'}>
-          A Decentralized Library, that allows users to upload files, retrieve files, share files with other users.
-          </Text>
+          <Box fontSize={{ base: 'md', lg: 'lg' }} color={'gray.200'}>
+            A Decentralized Library, that allows users to 
+            <Text 
+              fontSize={'lg'} 
+              color={'orange.500'} 
+              fontWeight={700}
+              letterSpacing={'2px'}
+            >{newText}</Text>
+          </Box>
           <Stack direction={{ base: 'column', md: 'row' }} spacing={4}>
             <Button name={"Get Started"} />
           </Stack>
         </Stack>
       </Flex>
-      <Flex flex={1}>
-        <Image
-          alt={'Login Image'}
-          objectFit={'contain'}
-          src={Hero}
-        />
+      <Flex overflow='hidden' maxW={'xl'} my='0' mx='auto'>
+        <Flex 
+        flex={1} 
+        whiteSpace={'nowrap'}
+        transition='ease 1000ms'
+        style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}>
+        {images.map((image, index) => (
+          <Image
+            display={'inline-block'}
+
+            alt={'Login Image'}
+            objectFit={'contain'}
+            key={index}
+            src={image}
+          />
+        ))}
+        </Flex>
       </Flex>
     </Stack>
   )
