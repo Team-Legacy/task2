a335f60f2e7dd228cb0416a43cd23a6fdf1d95e7
iyanuloluwa <iremide.nexzy@gmail.com>
2022-04-12 21:36:50 +0100

    image edit

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
 import { Box, Text,Image,Flex,Stack,Button } from "@chakra-ui/react"
+import images from "../../src/assets/city-g5543ba277_1920.jpg"
+import image_1 from "../../src/assets/eiffel-tower-g63e6beb8f_1920.jpg"
+import image_2 from "../../src/assets/japan-g17df8d80d_1920.jpg"
+import image_3 from "../../src/assets/lake-gd395dd1bd_1280.jpg"
+import image_4 from "../../src/assets/vehicles-g5e57e476e_1920.jpg"
+
+
+
 
 
 
@@ -12,7 +20,8 @@ const hero = () => {
         bgClip='text'
         fontSize='6xl'
         fontWeight='extrabold'
-        mt="120px"
+       
+
                 
         >
         The Best Books
@@ -21,9 +30,9 @@ const hero = () => {
       <Text color="black"fontSize="30px" fontWeight="md" pl="50px">
         upload your files
       </Text>
-      <Flex justifyContent="center" alignItems="center" pt="90px" flexWrap="wrap">
+      <Flex justifyContent="space-between" alignItems="center" pt="90px" flexWrap="wrap">
         <Box boxSize='sm' >
-          <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov' />
+          <Image src={images} alt='Dan Abramov' pl="10px" />
           <Text textAlign="center" pt="40px">Witches of the Wood</Text>
           <Stack spacing={4} direction='row' align='center' justify="center">
               <Button colorScheme='teal' size='lg' mt="40px">
@@ -32,48 +41,48 @@ const hero = () => {
 
             </Stack>          
         </Box>
-      <Box boxSize='sm'>
-        <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov' />
-        <Text textAlign="center" pt="40px">Witches of the Wood</Text>
-        <Stack spacing={4} direction='row' align='center' justify="center">
-            <Button colorScheme='teal' size='lg' mt="40px">
-              Read Through 
-            </Button>
+        <Box boxSize='sm'>
+          <Image src={image_1} alt='Dan Abramov' />
+          <Text textAlign="center" pt="40px">Witches of the Wood</Text>
+          <Stack spacing={4} direction='row' align='center' justify="center">
+              <Button colorScheme='teal' size='lg' mt="40px">
+                Read Through 
+              </Button>
 
-          </Stack>    
-      </Box>
-      <Box boxSize='sm'>
-        <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov' />
-        <Text textAlign="center" pt="40px">Witches of the Wood</Text>
-        <Stack spacing={4} direction='row' align='center' justify="center">
-            <Button colorScheme='teal' size='lg' mt="40px">
-              Read Through 
-            </Button>
+            </Stack>    
+        </Box>
+        <Box boxSize='sm'>
+          <Image src={image_2} alt='Dan Abramov' />
+          <Text textAlign="center" pt="40px">Witches of the Wood</Text>
+          <Stack spacing={4} direction='row' align='center' justify="center">
+              <Button colorScheme='teal' size='lg' mt="40px">
+                Read Through 
+              </Button>
 
-          </Stack>    
-      </Box>
-      <Box boxSize='sm'>
-        <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov' />
-        <Text textAlign="center" pt="40px">Witches of the Wood</Text>
-        <Stack spacing={4} direction='row' align='center' justify="center">
-            <Button colorScheme='teal' size='lg' mt="40px">
-              Read Through 
-            </Button>
+            </Stack>    
+        </Box>
+        <Box boxSize='sm'>
+          <Image src={image_3} alt='Dan Abramov' />
+          <Text textAlign="center" pt="40px">Witches of the Wood</Text>
+          <Stack spacing={4} direction='row' align='center' justify="center">
+              <Button colorScheme='teal' size='lg' mt="40px">
+                Read Through 
+              </Button>
 
-          </Stack>    
-      </Box>
-      <Box boxSize='sm'>
-        <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov' />
-        <Text textAlign="center" pt="40px">Witches of the Wood</Text>
-        <Stack spacing={4} direction='row' align='center' justify="center">
-            <Button colorScheme='teal' size='lg' mt="40px">
-              Read Through 
-            </Button>
+            </Stack>    
+        </Box>
+        <Box boxSize='sm'>
+        <Image src={image_4} alt='Dan Abramov' />
+          <Text textAlign="center" pt="40px">Witches of the Wood</Text>
+          <Stack spacing={4} direction='row' align='center' justify="center">
+              <Button colorScheme='teal' size='lg' mt="40px">
+                Read Through 
+              </Button>
 
-          </Stack>    
-      </Box>
+            </Stack>    
+        </Box>
 
-    </Flex>
+      </Flex>
 
 
     </Box>
